ca09cf164a5036d5dcd69de06fc2429f40311826
Bruno Azevedo <brunogmn751@gmail.com>
2026-08-16 16:08:16 -0300

    fixed pathing on tailwind import

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from './node_modules/@tailwindcss/vite'
 
 export default defineConfig({
     server: {
