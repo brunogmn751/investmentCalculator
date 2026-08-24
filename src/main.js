30a77f4f67efdbc91b88aef9d7e534b08bbe5f2a
Bruno Azevedo <brunogmn751@gmail.com>
2026-08-24 11:43:29 -0300

    Connected form to main.js file

--- src/main.js
+++ src/main.js
@@ -1,1 +1,22 @@
-function 
+import {generateReturnsArray} from "./investmentGoals"
+
+const calculateBtn = document.getElementById("calculate-btn");
+
+function renderProgression(){
+    const startingAmount = Number(document.querySelector("#initial-investment").value);
+    const timeAmount = Number(document.querySelector("#time-amount").value);
+    const additionalContribution = Number(document.querySelector("#additional-contributions").value);
+    const returnRate = Number(document.querySelector("#return-rate").value);
+    const taxRate = Number(document.querySelector("#tax-rate").value);
+    const returnRatePeriod = document.querySelector("#evaluation-period"). value;
+    const timePeriod = document.querySelector("#time-period-select").value;
+    
+    const returnsArray = generateReturnsArray(startingAmount, timeAmount, timePeriod,additionalContribution,returnRate,returnRatePeriod);
+
+    console.log(returnsArray)
+};
+
+calculateBtn.addEventListener('click', renderProgression);
+
+
+
