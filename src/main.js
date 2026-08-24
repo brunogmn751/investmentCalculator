e2b8704c263f19d2cef8050d2495e49e2a305a19
Bruno Azevedo <brunogmn751@gmail.com>
2026-08-24 16:57:14 -0300

    associated the calculate action to the form submission

--- src/main.js
+++ src/main.js
@@ -1,22 +1,35 @@
-import {generateReturnsArray} from "./investmentGoals"
+import { generateReturnsArray } from "./investmentGoals";
 
 const calculateBtn = document.getElementById("calculate-btn");
+const form = document.querySelector("#investment-form");
 
-function renderProgression(){
-    const startingAmount = Number(document.querySelector("#initial-investment").value);
-    const timeAmount = Number(document.querySelector("#time-amount").value);
-    const additionalContribution = Number(document.querySelector("#additional-contributions").value);
-    const returnRate = Number(document.querySelector("#return-rate").value);
-    const taxRate = Number(document.querySelector("#tax-rate").value);
-    const returnRatePeriod = document.querySelector("#evaluation-period"). value;
-    const timePeriod = document.querySelector("#time-period-select").value;
-    
-    const returnsArray = generateReturnsArray(startingAmount, timeAmount, timePeriod,additionalContribution,returnRate,returnRatePeriod);
+function renderProgression() {
+  const startingAmount = Number(
+    document.querySelector("#initial-investment").value,
+  );
+  const timeAmount = Number(document.querySelector("#time-amount").value);
+  const additionalContribution = Number(
+    document.querySelector("#additional-contributions").value,
+  );
+  const returnRate = Number(document.querySelector("#return-rate").value);
+  const taxRate = Number(document.querySelector("#tax-rate").value);
+  const returnRatePeriod = document.querySelector("#evaluation-period").value;
+  const timePeriod = document.querySelector("#time-period-select").value;
 
-    console.log(returnsArray)
-};
+  const returnsArray = generateReturnsArray(
+    startingAmount,
+    timeAmount,
+    timePeriod,
+    additionalContribution,
+    returnRate,
+    returnRatePeriod,
+  );
 
-calculateBtn.addEventListener('click', renderProgression);
+  console.log(returnsArray);
+}
 
-
-
+// calculateBtn.addEventListener("click", renderProgression);
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  renderProgression();
+});
